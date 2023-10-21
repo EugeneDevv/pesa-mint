c8df65387891b2e95031092a8315bfa4b7fcbcf6
Eugene Odhiambo <eugenemrlens@gmail.com>
2023-10-21 21:23:49 +0300

    first commit

---
+++ src/main/java/com/eugenedevv/pesamint/PesamintApplication.java
@@ -0,0 +1,13 @@
+package com.eugenedevv.pesamint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesamintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesamintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eugenedevv/pesamint/PesamintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugenedevv.pesamint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesamintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
